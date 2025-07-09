24187893aa08820631c2a1619c8c9bac785067d4
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-07-10 04:40:07 +0800

    Change the choice to Math.floor(Math.random() * 3 +1)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getComputerChoice() {
   // let rock = "rock";
   // let paper = "paper";
   // let scissor = "scissors";
-  let choice = Math.floor(Math.random() * 3);
+  let choice = Math.floor(Math.random() * 3 + 1);
   // console.log(Math.random());
   console.log(choice);
 }
