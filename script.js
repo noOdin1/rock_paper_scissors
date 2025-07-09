14fbd288885edcd32f0a289a69712be35a417dbe
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-07-10 04:26:10 +0800

    Add getComputerChoice function, testing Math.random()

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello, World!");
+
+function getComputerChoice() {
+  // let rock = "rock";
+  // let paper = "paper";
+  // let scissor = "scissors";
+  let choice = Math.random();
+  // console.log(Math.random());
+  console.log(choice);
+}
+
+getComputerChoice();
