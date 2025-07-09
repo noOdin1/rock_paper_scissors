b8eda477826f3cba6dcfdcb3978500c8bd3d3e94
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-07-10 04:46:57 +0800

    Cleaned by removing the comment/testing lines.

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 console.log("Hello, World!");
 
 function getComputerChoice() {
-  // let rock = "rock";
-  // let paper = "paper";
-  // let scissor = "scissors";
   let choice = Math.floor(Math.random() * 3 + 1);
-  // console.log(Math.random());
-  // console.log(choice == 1 ? "rock" : choice == 2 ? "paper" : "scissor");
   return choice == 1 ? "rock" : choice == 2 ? "paper" : "scissor";
 }
 
