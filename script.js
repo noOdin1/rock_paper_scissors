2dd411b6112e8bfd3036268d93661f72e34aaebe
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-07-10 04:48:12 +0800

    Removed Hello, World line

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello, World!");
-
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3 + 1);
   return choice == 1 ? "rock" : choice == 2 ? "paper" : "scissor";
