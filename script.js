c02379a683f26875a44e1a2cdd41c3f1046f4fec
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-07-10 16:35:18 +0800

    Added getHumanChoice function and printout for value

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function getComputerChoice() {
   return choice == 1 ? "rock" : choice == 2 ? "paper" : "scissor";
 }
 
+function getHumanChoice() {
+  const userInput = prompt(
+    "Numbers 1, 2 and 3 please.\n1 for rock\n2 for paper\n3 for scissor\nPlease enter your choice:",
+  );
+  return userInput;
+}
+
+console.log(getHumanChoice());
 console.log(getComputerChoice());
