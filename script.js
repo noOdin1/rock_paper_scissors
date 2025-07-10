f24d0ef8f7d48dd0067333e00e692a40a22f6d4c
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-07-10 16:38:25 +0800

    Added interpretChoice function to interpret numbers to words

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function interpretChoice(choice) {
+  return choice == 1 ? "rock" : choice == 2 ? "paper" : "scissor";
+}
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3 + 1);
   return choice == 1 ? "rock" : choice == 2 ? "paper" : "scissor";
@@ -7,7 +11,7 @@ function getHumanChoice() {
   const userInput = prompt(
     "Numbers 1, 2 and 3 please.\n1 for rock\n2 for paper\n3 for scissor\nPlease enter your choice:",
   );
-  return userInput;
+  return interpretChoice(userInput);
 }
 
 console.log(getHumanChoice());
