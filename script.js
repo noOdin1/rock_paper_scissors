af803e7fcd3f69d950ef7161f3657f0b5dc6b281
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-07-10 17:24:04 +0800

    Add a default value for human choice

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   const user_input = prompt(
-    "Numbers 1, 2 and 3 please.\n1 for rock\n2 for paper\n3 for scissor\nPlease enter your choice:",
+    "Numbers 1, 2 and 3 please.\n1 for rock\n2 for paper\n3 for scissor\nDefault value (1)\nPlease enter your choice:",
+    "1",
   );
   return interpretChoice(user_input);
 }
